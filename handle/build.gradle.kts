b4ce92cb6ab5d99d17afe4346514b92c8d1b1844
R. Brian Amesbury <r.brian.amesbury@gmail.com>
2019-06-29 00:30:30 -0600

    convert handle to gradle kts, remove proguard files

---
+++ handle/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+  id("com.android.library")
+  kotlin("android")
+  kotlin("kapt")
+}
+
+android {
+  compileSdkVersion(28)
+
+  lintOptions {
+    disable("RtlEnabled")
+    isWarningsAsErrors = true
+    isAbortOnError = true
+    xmlReport = false
+  }
+
+  testOptions {
+    unitTests.apply {
+      isReturnDefaultValues = true
+      isIncludeAndroidResources = true
+    }
+    execution = "ANDROIDX_TEST_ORCHESTRATOR"
+    animationsDisabled = true
+  }
+
+  defaultConfig {
+    minSdkVersion(24)
+    targetSdkVersion(28)
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    testInstrumentationRunnerArguments = mapOf("clearPackageData" to "true")
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  dataBinding {
+    isEnabled = true
+  }
+
+  sourceSets {
+    val sharedTestDir = "src/sharedTest/java"
+    getByName("test") {
+      java.srcDir(sharedTestDir)
+    }
+    getByName("androidTest") {
+      java.srcDir(sharedTestDir)
+    }
+  }
+
+}
+
+dependencies {
+
+  implementation(project(M.BINDINGS))
+  implementation(project(M.COMPONENTS))
+  implementation(project(M.DOMAIN))
+  implementation(project(M.EXTENSIONS))
+  implementation(project(M.IDS))
+  implementation(project(M.RES))
+  implementation(project(M.WINDOWS))
+
+  implementation(D.orgJetbrainsKotlinKotlinStandardLibrary)
+  implementation(D.orgKoinKoinAndroid)
+  implementation(D.orgKoinKoinAndroidxViewmodel)
+  implementation(D.androidxConstraintlayoutConstraintlayout)
+  implementation(D.androidxNavigationNavigationFragmentKtx)
+  implementation(D.androidxNavigationNavigationUiKtx)
+
+  testImplementation(D.androidxTestExtJunitKtx)
+  testImplementation(D.orgKoinKoinTest)
+  testImplementation(D.orgRobolectricRobolectric)
+
+  androidTestUtil(D.androidxTestOrchestrator)
+
+  androidTestImplementation(D.orgKoinKoinTest)
+  androidTestImplementation(D.androidxTestEspressoEspressoCore)
+  androidTestImplementation(D.androidxTestExtJunitKtx)
+
+}
