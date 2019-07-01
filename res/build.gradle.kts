20faeafa18d0e5e0f1ea04ec2e72f32b7f619475
R. Brian Amesbury <r.brian.amesbury@gmail.com>
2019-06-30 20:30:15 -0600

    convert res to gradle kts, remove proguard files

---
+++ res/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+  id("com.android.library")
+}
+
+android {
+  compileSdkVersion(28)
+
+  lintOptions {
+    isWarningsAsErrors = true
+    isAbortOnError = true
+    xmlReport = false
+  }
+
+  testOptions {
+    unitTests.apply {
+      isReturnDefaultValues = true
+      isIncludeAndroidResources = true
+    }
+    execution = "ANDROIDX_TEST_ORCHESTRATOR"
+    animationsDisabled = true
+  }
+
+  defaultConfig {
+    minSdkVersion(24)
+    targetSdkVersion(28)
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    testInstrumentationRunnerArguments = mapOf("clearPackageData" to "true")
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  sourceSets {
+    val sharedTestDir = "src/sharedTest/java"
+    getByName("test") {
+      java.srcDir(sharedTestDir)
+    }
+    getByName("androidTest") {
+      java.srcDir(sharedTestDir)
+    }
+  }
+
+}
+
+dependencies {
+
+  implementation(D.androidxAppcompatAppcompat)
+
+}
