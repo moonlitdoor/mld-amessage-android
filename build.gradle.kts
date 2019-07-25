9577eaeef474f6d0703a266565e698fdbe9a198a
R. Brian Amesbury <r.brian.amesbury@gmail.com>
2019-07-25 09:46:00 -0600

    updating android studio

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,9 @@ buildscript {
   dependencies {
     classpath("com.github.ben-manes:gradle-versions-plugin:0.21.0")
     classpath("com.moonlitdoor.git-version:git-version:0.1.1")
-    classpath("com.android.tools.build:gradle:3.6.0-alpha03")
+    classpath("com.android.tools.build:gradle:3.6.0-alpha05")
     classpath(kotlin("gradle-plugin", version = "1.3.41"))
-    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.1.0-alpha06")
+    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.1.0-beta02")
 //    classpath ("com.google.firebase:perf-plugin:1.2.1")
 //    classpath ("com.github.triplet.gradle:play-publisher:2.2.1")
 //    classpath ("io.fabric.tools:gradle:1.29.0")
@@ -30,6 +30,11 @@ allprojects {
     set("mediumTests", if (project.hasProperty("mediumTests")) project.property("mediumTests") else false)
     set("smallTests", if (project.hasProperty("smallTests")) project.property("smallTests") else false)
     set("flakyTests", if (project.hasProperty("flakyTests")) project.property("flakyTests") else false)
+  }
+  tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "1.8"
+    }
   }
   tasks.withType<Test> {
     testLogging.events("failed", "passed", "skipped")
