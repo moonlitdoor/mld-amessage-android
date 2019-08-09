fac40002a9b7aecb607d65a8f6a52a1c96cc958b
R. Brian Amesbury <r.brian.amesbury@gmail.com>
2019-08-09 08:35:22 -0600

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ buildscript {
     maven(url = "https://plugins.gradle.org/m2/")
   }
   dependencies {
-    classpath("com.github.ben-manes:gradle-versions-plugin:0.21.0")
+    classpath("com.github.ben-manes:gradle-versions-plugin:0.22.0")
     classpath("com.moonlitdoor.git-version:git-version:0.1.1")
     classpath("com.android.tools.build:gradle:3.6.0-alpha05")
     classpath(kotlin("gradle-plugin", version = "1.3.41"))
-    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.1.0-beta02")
+    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.0-alpha01")
 //    classpath ("com.google.firebase:perf-plugin:1.2.1")
 //    classpath ("com.github.triplet.gradle:play-publisher:2.2.1")
 //    classpath ("io.fabric.tools:gradle:1.29.0")
